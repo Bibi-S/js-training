8c36f1d3c335cdc98ea0118dcf8525d76cac60da
Bibi S <bkmso158@gmail.com>
2023-03-14 16:50:07 +0100

    feat: adds new functions

---
+++ script.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+console.log("Barbara");
+console.log("B" + "a" + "r" + "b" + "a" + "r" + "a");
+
+// Error! Make it work
+console.log("Hallo");
+// Error! Make it work
+console.log('That doesn"t work');
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+console.log(25 / 5 === 5 && 50 / 10 === 5);
+
+function welcomeMsg(personName) {
+  if (typeof personName === "string" && personName.length > 1) {
+    return "Welcome " + personName + "!";
+  } else {
+    return personName + " is not a name ";
+  }
+}
+
+console.log(welcomeMsg("Marc"));
+console.log(welcomeMsg("x"));
+
+function calcGrossPrice(netPrice, taxRate) {
+  const grossPrice = netPrice + netPrice * taxRate;
+  return grossPrice;
+}
+console.log(calcGrossPrice(20, 0.19));
+console.log(calcGrossPrice(40, 0.16));
+
+function addPositive(firstNum = Number, secondNum = Number) {
+  const result = Math.abs(firstNum) + Math.abs(secondNum);
+  return result;
+}
+console.log(addPositive(2, 3));
+console.log(addPositive(3, -5));
+console.log(addPositive(-1, -8));
+console.log(addPositive(-1.5, -7));
