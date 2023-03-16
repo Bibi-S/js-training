2a9b6f1bc99d3bbc6ea4d45c9b2e287edb628ed5
Bibi S <bkmso158@gmail.com>
2023-03-16 14:43:42 +0100

    feat: adds new functions

--- using-loops/script.js
+++ using-loops/script.js
@@ -25,16 +25,18 @@ function isString(parameter) {
 
 function oddNumbers(firstNum, secondNum) {
   // oddNumbers function
-
-  for (let index = firstNum; index <= secondNum; index++) {
+  let newOdd = "";
+  for (let index = 0; index <= secondNum; index++) {
     //let allNum = index;
     //console.log(allNum);
     if (index % 2 !== 0 && index >= 0) {
       /*  kann man als 2.Bedienung schreiben: && index === Math.abs(index)?? */
-      newOdd = index;
-      console.log(newOdd);
+      newOdd += index + ",";
+      //console.log(newOdd);
     }
   }
+
+  return newOdd.substring(0, newOdd.length - 1);
 }
 console.log(oddNumbers(-10, 5));
 
@@ -43,13 +45,14 @@ console.log(oddNumbers(-10, 5));
 function charCount(strWord, charWord) {
   //let strWord = "string";
   //let charWord = "string";
-  for (let index = strWord[0]; index <= strWord.length; index++) {
-    //let charaWord = index;
-    if (charWord === index) {
-      let countChar = index++;
-
-      console.log(countChar);
+  let countChar = 0;
+  for (let index = 0; index <= strWord.length; index++) {
+    // console.log(index);
+    if (charWord === strWord[index]) {
+      countChar++;
+      //console.log(countChar);
     }
   }
+  return countChar;
 }
 console.log(charCount("mama", "m"));
